933ac6f95903fdec64f34135fdc5adb611b3566e
Hasan Yousuf <hasan@hasanyousuf.com>
2015-11-22 16:32:50 +0100

    Added jsbeautifier

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,59 @@
-module.exports = function(grunt){
-//Project configuration
-grunt.initConfig({
-//It will make your node configurations available to use
-    pkg: grunt.file.readJSON('package.json'),
+module.exports = function(grunt) {
+    //Project configuration
+    grunt.initConfig({
+        //It will make your node configurations available to use
+        pkg: grunt.file.readJSON('package.json'),
 
-//JSHint configurations
-	jshint: {
-			//The paths to make the
-			//The paths tell JSHint which files to validate
-			all:['Gruntfile.js','src/*.js']
-		},
-	casperjs: {
-  		options: {
-    		casperjsOptions: [ '--no-colors', '--ssl-protocol=tlsv1','--verbose=true', '--xunit=artifacts/xunit.xml',
-    		'--includes=./lib/globalvars.js,./lib/custom_functions.js',
-    		'--server='+grunt.option("server"),
-    		'--username=' + grunt.option("username"),
-    		'--password='+ grunt.option("password"),
-    		'--log-level='+ grunt.option("logs")
- 	 ]},
-  		files: ['src/index.js']
-	},
-});
+        //JSHint configurations
+        jshint: {
+            //The paths to make the
+            //The paths tell JSHint which files to validate
+            all: ['Gruntfile.js', 'src/*.js']
+        },
+        casperjs: {
+            options: {
+                casperjsOptions: ['--no-colors', '--ssl-protocol=tlsv1', '--verbose=true', '--xunit=artifacts/xunit.xml',
+                    '--includes=./lib/globalvars.js,./lib/custom_functions.js',
+                    '--server=' + grunt.option("server"),
+                    '--username=' + grunt.option("username"),
+                    '--password=' + grunt.option("password"),
+                    '--log-level=' + grunt.option("logs")
+                ]
+            },
+            files: ['src/index.js']
+        },
+        jsbeautifier: {
+            files: ["src/**/*.js", "lib/*js", "Gruntfile.js"],
+            options: {
+                js: {
+                    braceStyle: "collapse",
+                    breakChainedMethods: false,
+                    e4x: false,
+                    evalCode: false,
+                    indentChar: " ",
+                    indentLevel: 0,
+                    indentSize: 4,
+                    indentWithTabs: false,
+                    jslintHappy: false,
+                    keepArrayIndentation: false,
+                    keepFunctionIndentation: false,
+                    maxPreserveNewlines: 10,
+                    preserveNewlines: true,
+                    spaceBeforeConditional: true,
+                    spaceInParen: false,
+                    unescapeStrings: false,
+                    wrapLineLength: 0,
+                    endWithNewline: true
+                }
+            }
+        },
+    });
 
-//Loading the plugins
-grunt.loadNpmTasks('grunt-casperjs');
-grunt.loadNpmTasks('grunt-contrib-jshint');
-//grunt.loadNpmTasks('jshint');
-grunt.registerTask('default',['jshint']);
+    //Loading the plugins
+    grunt.loadNpmTasks('grunt-casperjs');
+
+    grunt.loadNpmTasks("grunt-jsbeautifier");
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    //grunt.loadNpmTasks('jshint');
+    grunt.registerTask('default', ['jshint', 'jsbeautify']);
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 var x = require('casper').selectXPath;
-casper.options.viewportSize = {width: 1705, height: 962};
+casper.options.viewportSize = {
+    width: 1705,
+    height: 962
+};
 casper.on('page.error', function(msg, trace) {
-   this.echo('Error: ' + msg, 'ERROR');
-   for(var i=0; i<trace.length; i++) {
-       var step = trace[i];
-       this.echo('   ' + step.file + ' (line ' + step.line + ')', 'ERROR');
-   }
+    this.echo('Error: ' + msg, 'ERROR');
+    for (var i = 0; i < trace.length; i++) {
+        var step = trace[i];
+        this.echo('   ' + step.file + ' (line ' + step.line + ')', 'ERROR');
+    }
 });
 
 var fs = require("fs");
@@ -18,76 +21,78 @@ casper.echo("Casper CLI passed args");
 require("utils").dump(casper.cli.args);
 
 casper.test.begin('Headless tests', function(test) {
-   casper.start(server);
-   casper.waitForSelector("a:nth-child(2) img",
-       function success() {
-           test.assertExists("a:nth-child(2) img");
-           this.click("a:nth-child(2) img");
-       },
-       function fail() {
-           test.assertExists("a:nth-child(2) img");
-   });
-   casper.waitForSelector("form input[name='user']",
-       function success() {
-           test.assertExists("form input[name='user']");
-           this.click("form input[name='user']");
-       },
-       function fail() {
-           test.assertExists("form input[name='user']");
-   });
-   casper.waitForSelector("input[name='user']",
-       function success() {
-           this.sendKeys("input[name='user']", username);
-       },
-       function fail() {
-           test.assertExists("input[name='user']");
-   });
-   casper.waitForSelector("form input[name='passwd']",
-       function success() {
-           test.assertExists("form input[name='passwd']");
-           this.click("form input[name='passwd']");
-       },
-       function fail() {
-           test.assertExists("form input[name='passwd']");
-   });
-   casper.waitForSelector("input[name='passwd']",
-       function success() {
-           this.sendKeys("input[name='passwd']", password);
-       },
-       function fail() {
-           test.assertExists("input[name='passwd']");
-   });
+    casper.start(server);
+    casper.waitForSelector("a:nth-child(2) img",
+        function success() {
+            test.assertExists("a:nth-child(2) img");
+            this.click("a:nth-child(2) img");
+        },
+        function fail() {
+            test.assertExists("a:nth-child(2) img");
+        });
+    casper.waitForSelector("form input[name='user']",
+        function success() {
+            test.assertExists("form input[name='user']");
+            this.click("form input[name='user']");
+        },
+        function fail() {
+            test.assertExists("form input[name='user']");
+        });
+    casper.waitForSelector("input[name='user']",
+        function success() {
+            this.sendKeys("input[name='user']", username);
+        },
+        function fail() {
+            test.assertExists("input[name='user']");
+        });
+    casper.waitForSelector("form input[name='passwd']",
+        function success() {
+            test.assertExists("form input[name='passwd']");
+            this.click("form input[name='passwd']");
+        },
+        function fail() {
+            test.assertExists("form input[name='passwd']");
+        });
+    casper.waitForSelector("input[name='passwd']",
+        function success() {
+            this.sendKeys("input[name='passwd']", password);
+        },
+        function fail() {
+            test.assertExists("input[name='passwd']");
+        });
 
-   casper.waitForSelector("form input[type=submit][value='Log in']",
-       function success() {
-           test.assertExists("form input[type=submit][value='Log in']");
-           this.click("form input[type=submit][value='Log in']");
-       },
-       function fail() {
-           test.assertExists("form input[type=submit][value='Log in']");
-   });
+    casper.waitForSelector("form input[type=submit][value='Log in']",
+        function success() {
+            test.assertExists("form input[type=submit][value='Log in']");
+            this.click("form input[type=submit][value='Log in']");
+        },
+        function fail() {
+            test.assertExists("form input[type=submit][value='Log in']");
+        });
 
-   /* submit form */
-   casper.waitForSelector(x("//a[normalize-space(text())='Patients']"),
-       function success() {
-           test.assertExists(x("//a[normalize-space(text())='Patients']"));
-           this.click(x("//a[normalize-space(text())='Patients']"));
-       },
-       function fail() {
-           test.assertExists(x("//a[normalize-space(text())='Patients']"));
-   });
-casper.then(function(){
-this.capture("screen.png");
+    /* submit form */
+    casper.waitForSelector(x("//a[normalize-space(text())='Patients']"),
+        function success() {
+            test.assertExists(x("//a[normalize-space(text())='Patients']"));
+            this.click(x("//a[normalize-space(text())='Patients']"));
+        },
+        function fail() {
+            test.assertExists(x("//a[normalize-space(text())='Patients']"));
+        });
+    casper.then(function() {
+        this.capture("screen.png");
 
-});
-   casper.waitForSelector("select",
-       function success() {
-           test.assertExists("select");
-           this.click("select");
-       },
-       function fail() {
-           test.assertExists("select");
-   });
+    });
+    casper.waitForSelector("select",
+        function success() {
+            test.assertExists("select");
+            this.click("select");
+        },
+        function fail() {
+            test.assertExists("select");
+        });
 
-   casper.run(function() {test.done();});
+    casper.run(function() {
+        test.done();
+    });
 });
